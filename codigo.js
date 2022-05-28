2ca47de2320dd80a563bb2b51d31dae0592bbe6c
HarolPJ <99227654+HarolPJ@users.noreply.github.com>
2022-05-27 23:26:49 -0500

    Add files via upload

---
+++ codigo.js
@@ -0,0 +1,107 @@
+document.addEventListener('DOMContentLoader', () =>{
+    
+})
+
+$(".arqui").click(function(){
+    $(".arqui").addClass('resaltar');
+   
+
+});
+
+$(".java").click(function(){
+    $(".java").addClass('resaltar');
+   
+
+});
+
+$(".web").click(function(){
+    $(".web").addClass('resaltar');
+   
+
+});
+
+$(".figma").click(function(){
+    $(".figma").addClass('resaltar');
+   
+
+});
+
+$(".etiqueta").click(function(){
+    $(".etiqueta").addClass('resaltar');
+   
+
+});
+
+$(".github").click(function(){
+    $(".github").addClass('resaltar');
+   
+
+});
+
+$(".body").click(function(){
+    $(".body").addClass('resaltar');
+   
+
+});
+
+$(".api").click(function(){
+    $(".api").addClass('resaltar');
+   
+
+});
+
+$(".jquery").click(function(){
+    $(".jquery").addClass('resaltar');
+   
+
+});
+
+$(".html").click(function(){
+    $(".html").addClass('resaltar');
+   
+
+});
+
+$(".git").click(function(){
+    $(".git").addClass('resaltar');
+   
+
+});
+
+$(".css").click(function(){
+    $(".css").addClass('resaltar');
+   
+
+});
+
+$(".head").click(function(){
+    $(".head").addClass('resaltar');
+   
+
+});
+
+$(".services").click(function(){
+    $(".services").addClass('resaltar');
+   
+
+});
+
+$(".dom").click(function(){
+    $(".dom").addClass('resaltar');
+   
+
+});
+
+$(".programa").click(function(){
+    $(".programa").addClass('resaltar');
+   
+
+});
+
+$(".maqueta").click(function(){
+    $(".maqueta").addClass('resaltar');
+   
+
+});
+
+
